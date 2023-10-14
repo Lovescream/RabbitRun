9da8cb413e69a342af88175d774353794cbeff5d
min-vely <jeongmin322@naver.com>
2023-10-14 21:48:30 +0900

    Made CarrotSpawner

---
+++ RabbitRun/Assets/Scripts/Models/CarrotRespawn.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarrotRespawn : MonoBehaviour
+{
+    public List<GameObject> CarrotPool = new List<GameObject>();
+    public GameObject[] Carrots;
+    // 카메라 뷰 하나에 나오는 최대 당근 갯수
+    public int objCnt = 1;
+
+    void Awake()
+    {
+        for (int i = 0; i < Carrots.Length; i++)
+        {
+            for (int j = 0; j < objCnt; j++)
+            {
+                CarrotPool.Add(CreateObj(Carrots[i], transform));
+            }
+        }
+    }
+
+    private void Start()
+    {
+        StartCoroutine(CreateObj());
+    }
+
+    IEnumerator CreateObj()
+    {
+        while (true)
+        {
+            CarrotPool[Random.Range(0, CarrotPool.Count)].SetActive(true);
+            yield return new WaitForSeconds(Random.Range(1.5f, 3.5f));
+        }
+    }
+
+    GameObject CreateObj(GameObject obj, Transform parent)
+    {
+        GameObject copy = Instantiate(obj);
+        copy.transform.SetParent(parent);
+        copy.SetActive(false);
+        return copy;
+    }
+
+
+}
---
+++ RabbitRun/Assets/Scripts/Models/Carrots.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Carrots : MonoBehaviour
+{
+    // 당근이 움직이는 속도, 1.5f 정도가 적당함
+    public float carrotSpeed = 0.0f;
+    // 당근 스폰 시작지점, 오른쪽 맨 끝은 (12, 1.05)
+    public Vector2 StartPosition;
+
+    private void OnEnable()
+    {
+        transform.position = StartPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.left * Time.deltaTime * carrotSpeed);
+
+        // 당근이 화면 밖으로 나가면 당근 오브젝트 비활성화
+        if (transform.position.x < -12f)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
