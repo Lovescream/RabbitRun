b0d401b671a37e2dc0cff8cc71b98be30088408d
asd <114643897+Lovescream@users.noreply.github.com>
2023-10-18 13:32:18 +0900

    당근 잘먹는다~

--- RabbitRun/Assets/Scripts/Models/Carrot.cs
+++ RabbitRun/Assets/Scripts/Models/Carrot.cs
@@ -2,44 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Carrot : MonoBehaviour
-{
-    int type;
-    public float speed = 0.015f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        type = Random.Range(1, 3);
-        // 당근 높낮이에 따라 시작점이 다름
-        if (type == 1)
-        {
-            // 평지에 있는 당근
-            transform.position = new Vector3(12f, 1.05f, 0f);
-        }
-        else// if (type == 2)
-        {
-            // 점프하는 높이에 있는 당근
-            transform.position = new Vector3(12f, 4f, 0f);
+public enum CarrotHeight {
+    High,
+    Low,
+}
+
+public class Carrot : MonoBehaviour {
+
+    #region Fields
+
+    private float speed;
+
+    #endregion
+
+    #region MonoBehaviours
+
+    void Update() {
+        transform.position += new Vector3(-speed * Time.deltaTime, 0f, 0f);
+        if (this.transform.position.x < -12f) {
+            Destroy(this.gameObject);
         }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        transform.position += new Vector3(-speed, 0f, 0f);
-        // 당근이 화면 왼쪽 밖으로 넘어가면
-        if (transform.position.x < -12f)
-        {
-            Destroy(gameObject);
-        }
-    }
-
-    void OnCollisionEnter2D(Collision2D coll)
-    {
-        if (coll.gameObject.tag == "Player")
-        {
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.gameObject.CompareTag("Player")) {            // Tag 비교는 CompareTag를 사용하는 것이 낫습니다!
             Debug.Log("냠");
-            Destroy(gameObject);
+            Destroy(this.gameObject);
         }
     }
+
+    #endregion
+
+    #region Initialize
+
+    public void Initialize(CarrotHeight height, float speed) {
+        this.transform.position = new(12f, height switch { CarrotHeight.High => 4f, CarrotHeight.Low => 1.05f, _ => 4f });      // height 타입에 따라 4f 또는 1.05f의 y값을 가짐.
+        this.speed = speed;
+    }
+
+    #endregion
+
 }
--- RabbitRun/Assets/Scripts/Models/CarrotSpawn.cs
+++ RabbitRun/Assets/Scripts/Models/CarrotSpawn.cs
@@ -2,25 +2,55 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CarrotSpawn : MonoBehaviour
-{
-    public GameObject carrot;
-    // Start is called before the first frame update
-    void Start()
-    {
-        InvokeRepeating("carrotRespawn", 0, Random.Range(1.5f, 3.5f));
+public class CarrotSpawn : MonoBehaviour{
+
+    #region Inspector
+
+    [Header("Prefabs")]
+    [SerializeField]
+    private Carrot carrotPrefab;
+    [Header("Games")]
+    [SerializeField]
+    private float carrotIntervalMin;
+    [SerializeField]
+    private float carrotIntervalMax;
+    [SerializeField]
+    private float carrotSpeedMin;
+    [SerializeField]
+    private float carrotSpeedMax;
+
+    #endregion
+
+    #region Fields
+
+    private float carrotSpawnTimer;
+
+    private Transform carrotParent;
+
+    #endregion
+
+    #region MonoBehaviours
+
+    void Start() {
+        carrotParent = GameObject.Find("CarrotSpawn").transform;        // Find는 부하를 많이 주기 때문에 최소한으로 쓰는 게 좋습니다!
+        carrotSpawnTimer = Random.Range(carrotIntervalMin, carrotIntervalMax);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        // Timer가 0이 되면 당근 소환.
+        carrotSpawnTimer -= Time.deltaTime;
+        if (carrotSpawnTimer <= 0) {
+            carrotSpawnTimer = Random.Range(carrotIntervalMin, carrotIntervalMax);
+            Carrot newCarrot = Instantiate(carrotPrefab);
+            newCarrot.transform.SetParent(carrotParent);                // parent = ~~ 보다는 SetParent(___)를 쓰는 게 낫습니다!
+
+            CarrotHeight height = Random.Range(0, 2) == 0 ? CarrotHeight.High : CarrotHeight.Low;
+            float speed = Random.Range(carrotSpeedMin, carrotSpeedMax);
+
+            newCarrot.Initialize(height, speed);
+        }
     }
 
-    void carrotRespawn()
-    {
-        // 당근을 CarrotSpawn 밑에 생성하기
-        GameObject newCarrot = Instantiate(carrot);
-        newCarrot.transform.parent = GameObject.Find("CarrotSpawn").transform;
-    }
+    #endregion
+
 }
